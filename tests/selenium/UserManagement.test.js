94c663448c3be0a74196cbc98a153f3b4278755b
molmardo <molnarmark2005@gmail.com>
2025-04-29 11:24:06 +0000

    UserManagement.test.js  Waiting for the first row of the table to load

--- tests/selenium/UserManagement.test.js
+++ tests/selenium/UserManagement.test.js
@@ -2,17 +2,27 @@ const { Builder, By, until } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 
 (async function userManagementTest() {
-  let driver = await new Builder().forBrowser('chrome').build();
+    let driver = await new Builder().forBrowser('chrome').build();
 
-  try {
-    await driver.get('http://checkitoutlite.test/admin/users');
+    try {
+        await driver.get('http://checkitoutlite.test/admin/users');
 
-    await driver.wait(until.elementLocated(By.css('h2')), 10000);
+        // Waiting for the page to load
+        await driver.wait(until.elementLocated(By.css('h2')), 10000);
 
+        const pageTitle = await driver.findElement(By.css('h2')).getText();
+        if (pageTitle.includes('Felhasználók kezelése')) {
+            console.log('Page loaded successfully.');
+        } else {
+            throw new Error('Wrong page loaded.');
+        }
 
-  } catch (err) {
-    console.error('Error:', err);
-  } finally {
-    await driver.quit();
-  }
+        // Waiting for the first row of the table to load
+        await driver.wait(until.elementLocated(By.css('table tbody tr')), 10000);
+
+    } catch (err) {
+        console.error('Error:', err);
+    } finally {
+        await driver.quit();
+    }
 })();
